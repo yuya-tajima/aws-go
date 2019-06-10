362e55247e28c1488167a25d0808f33c6e6fcd34
Yuya Tajima <tajimataso@gmail.com>
2019-06-10 15:45:17 +0000

    Add auth paramaters to API Server

--- aws/util/util.go
+++ aws/util/util.go
@@ -7,6 +7,11 @@ import (
 	_awserr "github.com/aws/aws-sdk-go/aws/awserr"
 )
 
+type StatusRes struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
 func Errorf(msg string, args ...interface{}) {
 	_errorf(msg, args)
 }
@@ -31,4 +36,26 @@ func _error(err error) {
 		// Message from an error.
 		_errorf(err.Error())
 	}
+}
+
+func GetErrorResponse(err error) (int, *StatusRes) {
+
+	var res *StatusRes
+	var sCode = 400
+
+	if awsErr, ok := err.(_awserr.Error); ok {
+		res = &StatusRes{
+			Code:    awsErr.Code(),
+			Message: awsErr.Message(),
+		}
+		if reqErr, ok := err.(_awserr.RequestFailure); ok {
+			res = &StatusRes{
+				Code:    reqErr.Code(),
+				Message: reqErr.Message(),
+			}
+			sCode = reqErr.StatusCode()
+		}
+	}
+
+	return sCode, res
 }
--- httpd/api/main.go
+++ httpd/api/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	_ "fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 	"github.com/yuya-tajima/aws-go/aws"
+	"github.com/yuya-tajima/aws-go/aws/util"
 )
 
 var _aws *aws.Aws
@@ -14,18 +14,27 @@ func main() {
 
 	e := echo.New()
 
-	_aws = &aws.Aws{}
-	_aws.SetSession("tajima")
-	_aws.SetEc2Client()
-
 	e.GET("/ec2/desc", func(c echo.Context) error {
-		result, _ := _aws.Ec2.GetInstances("")
 
-		var sCode int
+		h := c.Request().Header
 
-		sCode = http.StatusOK
+		_aws = &aws.Aws{}
+		_aws.SetStaticSession(h.Get("Secret_Id"), h.Get("Secret_key"), "", "")
 
-		return c.JSON(sCode, result)
+		cfg := &aws.Ec2Config{
+			Region: "ap-northeast-1",
+		}
+
+		_aws.SetEc2Client(cfg)
+
+		result, err := _aws.Ec2.GetInstances("")
+
+		if err != nil {
+			errCode, errRes := util.GetErrorResponse(err)
+			return c.JSON(errCode, errRes)
+		} else {
+			return c.JSON(http.StatusOK, result)
+		}
 	})
 
 	e.Logger.Fatal(e.Start(":8081"))
